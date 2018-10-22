a8678cc3473e6a3221e67ec30c4b1ef65d253fb3
unknown <you@example.com>
2018-10-22 21:30:45 +0200

    Modół 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[]args ){
-        System.out.println("Modół 6. Wprowadzenie do testowania oprogramowania");
+    public static void main (String[] args) {
+
+        Calculator c = new Calculator();
+        int result = c.add(20, 15);
+        if (result == 35) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Add test error");
+        }
+
+        result = c.substract(20, 15);
+        if(result == 5) {
+            System.out.println("Substract test OK");
+        } else {
+            System.out.println("Substract test error");
+        }
     }
 }
