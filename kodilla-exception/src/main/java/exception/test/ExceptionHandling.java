881357121338e03f28882014716f7c0c1ee77741
unknown <you@example.com>
2018-11-26 21:40:19 +0100

    8.3

--- kodilla-exception/src/main/java/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/exception/test/ExceptionHandling.java
@@ -1,12 +1,14 @@
 package exception.test;
 
 public class ExceptionHandling {
+        public static void main (String[] args) {
 
-    SecondChallange secondChallange = new SecondChallange();
-    try {
-        secondChallange.probablyIWillThrowException(1.2, 1.5);
-    } catch (Exception e) {
-        e.printStackTrace();
+        SecondChallange secondChallange = new SecondChallange();
+        try {
+            secondChallange.probablyIWillThrowException(1.2, 1.5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
