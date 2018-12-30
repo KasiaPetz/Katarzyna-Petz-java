aa9761f5e59acd44ca37ca802417e2971c839bda
unknown <you@example.com>
2018-12-30 18:50:39 +0100

    11.3

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/AggressivePredictor.java
@@ -0,0 +1,9 @@
+package com.kodilla.patterns.strategy;
+
+public class AggressivePredictor implements BuyPredictor {
+
+    @Override
+    public String predictWhatToBuy() {
+        return "[Agressive predictor] Buy stock of XYZ";
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/social/UserTestSuite.java
@@ -0,0 +1,47 @@
+package com.kodilla.patterns.social;
+
+import com.kodilla.patterns.strategy.social.Millenials;
+import com.kodilla.patterns.strategy.social.SnapchatPublisher;
+import com.kodilla.patterns.strategy.social.YGeneration;
+import com.kodilla.patterns.strategy.social.ZGeneration;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+    @Test
+    public void testDefalutSharingStrategies() {
+        //Given
+        Millenials ewa = new Millenials("Ewa");
+        YGeneration marek = new YGeneration("Marek");
+        ZGeneration paulina = new ZGeneration("Paulina");
+
+        //When
+        String ewaPosting = ewa.sharePost();
+        System.out.println("Ewa " + ewaPosting);
+        String marekPosting = marek.sharePost();
+        System.out.println("Marek " + marekPosting);
+        String paulinaPosting = paulina.sharePost();
+        System.out.println("Paulina " + paulinaPosting);
+
+        //Then
+        Assert.assertEquals("published new post on Facebook", ewaPosting );
+        Assert.assertEquals("published new tweet on Twitter", marekPosting);
+        Assert.assertEquals("published new snap on Snapchat", paulinaPosting);
+    }
+
+    @Test
+    public void testIndividualSharingStrategy() {
+        //Given
+        Millenials ewa = new Millenials("Ewa");
+
+        //When
+        String ewaPosting = ewa.sharePost();
+        System.out.println("Ewa " + ewaPosting);
+        ewa.setPublishPreferences(new SnapchatPublisher());
+        ewaPosting = ewa.sharePost();
+        System.out.println("Ewa tried something new and " + ewaPosting);
+
+        //Then
+        Assert.assertEquals("published new snap on Snapchat", ewaPosting);
+    }
+}
