c90faf915481c3ea439fc5a2a06425855f43146d
unknown <you@example.com>
2018-12-30 19:05:10 +0100

    11.3

--- kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -1,4 +1,4 @@
-package com.kodilla.patterns.social;
+package com.kodilla.patterns.strategy.social;
 
 import com.kodilla.patterns.strategy.social.Millenials;
 import com.kodilla.patterns.strategy.social.SnapchatPublisher;
